774592fd571626ed563bf370eb7f4b72b799ad5d
srividya-p <srividya.ssa@gmail.com>
2021-09-25 11:17:50 +0530

    Added Location Tab Functionality

--- app/src/main/java/com/example/pilltracker/DashboardActivity.java
+++ app/src/main/java/com/example/pilltracker/DashboardActivity.java
@@ -70,52 +70,11 @@ public class DashboardActivity extends AppCompatActivity {
         locationTab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-                if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
-                    switchOnGPS();
-                } else {
-                    getLocation();
-                }
+                String url = "http://maps.google.co.uk/maps?q=Pharmacy&hl=en";
+                Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse(url));
+                intent.setClassName("com.google.android.apps.maps","com.google.android.maps.MapsActivity");
+                startActivity(intent);
             }
         });
-    }
-
-    public void switchOnGPS() {
-        System.out.println("switchOnFunction");
-        final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-
-        dialogBuilder.setMessage("GPS is not enabled. Enable it from settings?").setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-            }
-        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.cancel();
-            }
-        });
-
-        final AlertDialog alertDialog = dialogBuilder.create();
-        alertDialog.show();
-    }
-
-    public void getLocation() {
-        if (ActivityCompat.checkSelfPermission(
-                DashboardActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                DashboardActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
-        } else {
-            Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
-            if(locationGPS != null){
-                latitude = String.valueOf(locationGPS.getLatitude());
-                longitude = String.valueOf(locationGPS.getLongitude());
-                System.out.println(latitude+"\n"+longitude);
-            } else {
-                Toast.makeText(this, "Unable to find location.", Toast.LENGTH_SHORT).show();
-            }
-        }
-
     }
 }
