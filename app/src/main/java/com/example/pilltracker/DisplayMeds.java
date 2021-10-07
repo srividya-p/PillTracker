eeab1b5b25991d036b7e5d83b469c35b182dc144
AnyaGupta12 <58083673+AnyaGupta12@users.noreply.github.com>
2021-10-08 02:26:55 +0530

    Display Meds UI

---
+++ app/src/main/java/com/example/pilltracker/DisplayMeds.java
@@ -0,0 +1,14 @@
+package com.example.pilltracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class display_meds extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_meds);
+    }
+}
